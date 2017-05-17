ffaf77e737ef22f2a5099a07a5bac1ca60dc1125
Brian C. Lane <bcl@redhat.com>
2017-05-17 16:09:27 -0700

    Fix project name in license. Should be bdcs-cli

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,19 @@
 -- Copyright (C) 2017 Red Hat, Inc.
 --
--- This file is part of bdcs-api-server.
+-- This file is part of bdcs-cli.
 --
--- bdcs-api-server is free software: you can redistribute it and/or modify
+-- bdcs-cli is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
 -- the Free Software Foundation, either version 3 of the License, or
 -- (at your option) any later version.
 --
--- bdcs-api-server is distributed in the hope that it will be useful,
+-- bdcs-cli is distributed in the hope that it will be useful,
 -- but WITHOUT ANY WARRANTY; without even the implied warranty of
 -- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 -- GNU General Public License for more details.
 --
 -- You should have received a copy of the GNU General Public License
--- along with bdcs-api-server.  If not, see <http://www.gnu.org/licenses/>.
+-- along with bdcs-cli.  If not, see <http://www.gnu.org/licenses/>.
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
